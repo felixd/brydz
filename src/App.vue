939c05580af29f5e5a12cb78b5c8aa23909a4c2a
felixd <felixd@konopnickiej.com>
2022-09-12 16:38:27 +0200

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ const formData = ref(null);
 
 const getData = async () => {
   try {
-    const { data } = await fetch(
+    const data = await fetch(
       `https://register.brydz.gniezno.pl/cezar/?pid=${pid.value}`
     );
     console.log(data);
@@ -24,22 +24,3 @@ const getData = async () => {
   }
 };
 </script>
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
