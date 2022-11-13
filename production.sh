c3b176dcc08a922e9210e61ac014477a71217a96
Paweł Wojciechowski <felixd@konopnickiej.com>
2022-11-13 19:58:55 +0100

    bash

--- production.sh
+++ production.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+echo "Files to root:root"
 chown root:root * -R
 git pull
 
+echo "Rejestracja 1"
 cd rejestracja1
 npm run build
 mkdir dist/app
+echo "Rejestracja 2 PHP"
 cp ../php_app/*.php dist/app
 cd ..
 
+echo "Rejestracja 2"
 cd rejestracja2
 npm run build
 mkdir dist/app
+echo "Rejestracja 2 PHP"
 cp ../php_app/*.php dist/app
 cd ..
 
+echo "Files to www-data:www-data"
 chown -R www-data:www-data *
